3e37a27c68a3e8cd69fe65c28d65e0a263738914
Mariama310 <sokhna-mariama-bousso.kane@edu.devinci.fr>
2024-03-12 16:16:51 +0100

    update

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -2,16 +2,35 @@ import bodyParser from "body-parser";
 import express from "express";
 import { BASE_ONION_ROUTER_PORT } from "../config";
 
+// Ces variables doivent être accessibles dans la portée de la fonction simpleOnionRouter
+// et de toutes les routes, donc on les déclare ici.
+let lastReceivedEncryptedMessage: string | null = null;
+let lastReceivedDecryptedMessage: string | null = null;
+let lastMessageDestination: number | null = null;
+
 export async function simpleOnionRouter(nodeId: number) {
   const onionRouter = express();
   onionRouter.use(express.json());
   onionRouter.use(bodyParser.json());
 
-  // TODO implement the status route
   onionRouter.get("/status", (req, res) => {
     res.send("live");
   });
 
+  // Route pour obtenir le dernier message reçu crypté
+  onionRouter.get("/getLastReceivedEncryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedEncryptedMessage });
+  });
+
+  // Route pour obtenir le dernier message reçu décrypté
+  onionRouter.get("/getLastReceivedDecryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedDecryptedMessage });
+  });
+
+  // Route pour obtenir la destination du dernier message
+  onionRouter.get("/getLastMessageDestination", (req, res) => {
+    res.json({ result: lastMessageDestination });
+  });
 
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
     console.log(
